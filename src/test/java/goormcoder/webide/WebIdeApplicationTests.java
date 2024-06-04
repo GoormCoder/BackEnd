cc5204e015dc16c7f9a95231788438fa7dc2a2e9
Jinsu Choi <jschoi9160@naver.com>
2024-06-04 18:07:08 +0900

    Start Project!

---
+++ src/test/java/goormcoder/webide/WebIdeApplicationTests.java
@@ -0,0 +1,13 @@
+package goormcoder.webide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebIdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
