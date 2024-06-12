bc37bcaeeb47e385fb995908a12187ff248e2017
chaewonkim <kimlove9182@gmail.com>
2024-06-13 01:47:50 +0900

    feat: 삭제된 게시글 제외하고 조회하는 메서드 구현

--- src/main/java/goormcoder/webide/repository/BoardRepository.java
+++ src/main/java/goormcoder/webide/repository/BoardRepository.java
@@ -7,10 +7,17 @@ import goormcoder.webide.exception.NotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     default Board findByIdOrThrow(final Long boardId) {
-        return findById(boardId).orElseThrow(() -> new NotFoundException(ErrorMessage.BOARD_NOT_FOUND));
+        return findByIdAndDeletedAtIsNull(boardId).orElseThrow(() -> new NotFoundException(ErrorMessage.BOARD_NOT_FOUND));
     }
+
+    Optional<Board> findByIdAndDeletedAtIsNull(Long boardId);
+
+    List<Board> findAllByDeletedAtIsNull();
 }
