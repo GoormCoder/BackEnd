4073e1eb074dec59e32aa2324d2bf4933d006bee
ISMINIMIN <summmm.224@gmail.com>
2024-06-13 20:03:45 +0900

    feat: 도메인 설계

--- src/main/java/goormcoder/webide/domain/Member.java
+++ src/main/java/goormcoder/webide/domain/Member.java
@@ -1,13 +1,14 @@
 package goormcoder.webide.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import goormcoder.webide.domain.enumeration.Gender;
+import goormcoder.webide.domain.enumeration.MemberRole;
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Entity
 @NoArgsConstructor
@@ -19,5 +20,62 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;
+
+    @Column(name = "member_login_id", nullable = false, unique = true)
+    private String loginId;
+
+    @Column(name = "member_pw", nullable = false)
+    private String password;
+
+    @Column(name = "member_nick", nullable = false)
+    private String nick;
+
+    @Column(name = "member_name", nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "member_role", nullable = false)
+    private MemberRole role;
+
+    @Column(name = "member_birth", nullable = false)
+    private LocalDate birth;
+
+    @Column(name = "member_email", unique = true)
+    private String email;
+
+    @Column(name = "member_info")
+    private String info;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "member_gender", nullable = false)
+    private Gender gender;
+
+    @Builder
+    private Member(String loginId, String password, String nick, String name, MemberRole role, LocalDate birth, String email, String info, Gender gender) {
+        this.loginId = loginId;
+        this.password = password;
+        this.nick = nick;
+        this.name = name;
+        this.role = role;
+        this.birth = birth;
+        this.email = email;
+        this.info = info;
+        this.gender = gender;
+    }
+
+    public static Member of(String loginId, String password, String nick, String name, MemberRole role,
+                            LocalDate birth, String email, String info, Gender gender) {
+        return Member.builder()
+                .loginId(loginId)
+                .password(password)
+                .nick(nick)
+                .name(name)
+                .role(role)
+                .birth(birth)
+                .email(email)
+                .info(info)
+                .gender(gender)
+                .build();
+    }
 
 }
