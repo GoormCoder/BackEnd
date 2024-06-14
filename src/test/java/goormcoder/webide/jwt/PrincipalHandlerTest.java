ca42f56aa50fa018d27b4f5a9d4c67755cf58de4
ISMINIMIN <summmm.224@gmail.com>
2024-06-14 17:53:45 +0900

    test: PrincipalHandler 단위 테스트

---
+++ src/test/java/goormcoder/webide/jwt/PrincipalHandlerTest.java
@@ -0,0 +1,57 @@
+package goormcoder.webide.jwt;
+
+import goormcoder.webide.common.dto.ErrorMessage;
+import goormcoder.webide.exception.UnauthorizedException;
+import goormcoder.webide.security.MemberDetails;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PrincipalHandlerTest {
+
+    @Mock
+    private MemberDetails memberDetails;
+
+    private PrincipalHandler principalHandler;
+
+    @BeforeEach
+    void init() {
+        principalHandler = new PrincipalHandler();
+        setSecurityContext(memberDetails);
+    }
+
+    @Test
+    void getMemberLoginId() {
+        when(memberDetails.getUsername()).thenReturn("test-member");
+        String loginId = principalHandler.getMemberLoginId();
+        assertEquals("test-member", loginId);
+    }
+
+    @Test
+    void testGetMemberDetails_Unauthorized() {
+        SecurityContextHolder.clearContext();
+
+        UnauthorizedException exception = assertThrows(UnauthorizedException.class, () -> {
+            principalHandler.getMemberLoginId();
+        });
+
+        assertEquals(ErrorMessage.JWT_UNAUTHORIZED_EXCEPTION.getMessage(), exception.getMessage());
+    }
+
+    private void setSecurityContext(MemberDetails memberDetails) {
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(new UsernamePasswordAuthenticationToken(memberDetails, null));
+        SecurityContextHolder.setContext(securityContext);
+    }
+
+}
