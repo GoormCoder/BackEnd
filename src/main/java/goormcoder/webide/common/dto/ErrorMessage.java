df0b3ce32a5380ca1c382b8e63aa680644349209
ISMINIMIN <summmm.224@gmail.com>
2024-06-14 17:53:31 +0900

    feat: 인증된 사용자 정보를 가져오는 PrincipalHandler 구현

--- src/main/java/goormcoder/webide/common/dto/ErrorMessage.java
+++ src/main/java/goormcoder/webide/common/dto/ErrorMessage.java
@@ -12,6 +12,7 @@ public enum ErrorMessage {
     QUESTION_NOT_FOUND("해당하는 문제가 존재하지 않습니다."),
     BOARD_NOT_FOUND("해당하는 게시글이 존재하지 않습니다."),
     COMMENT_NOT_FOUND("해당하는 댓글이 존재하지 않습니다."),
+    JWT_UNAUTHORIZED_EXCEPTION("사용자 검증을 실패하였습니다."),
     ;
 
     private final String message;
