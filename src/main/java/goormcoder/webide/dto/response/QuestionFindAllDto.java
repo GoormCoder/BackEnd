ddcddec1bf4cbbbd994860af2386d4b698817432
Jinsu Choi <jschoi9160@naver.com>
2024-06-17 16:10:12 +0900

    style : } 누락 수정 및 변경된 변수명 적용
    - QuestionController } 누락 수정
    - QuestionFindAllDto 변수명 수정

--- src/main/java/goormcoder/webide/dto/response/QuestionFindAllDto.java
+++ src/main/java/goormcoder/webide/dto/response/QuestionFindAllDto.java
@@ -5,20 +5,20 @@ import goormcoder.webide.domain.Question;
 import java.util.List;
 
 public record QuestionFindAllDto(
-        long questionNum,
-        String questionTitle,
-        int questionLevel,
-        int questionSolved,
-        int questionAccuracy
+        long id,
+        String title,
+        int level,
+        int solved,
+        int accuracy
 ) {
         // solved 및 accuracy 는 추후에 로직 추가
         public static List<QuestionFindAllDto> listOf(List<Question> questions) {
         return questions
                 .stream()
                 .map(question -> new QuestionFindAllDto(
-                        question.getQuestionNum(),
-                        question.getQuestionTitle(),
-                        question.getQuestionLevel(),
+                        question.getId(),
+                        question.getTitle(),
+                        question.getLevel(),
                         1000,
                         90
                 )).toList();
