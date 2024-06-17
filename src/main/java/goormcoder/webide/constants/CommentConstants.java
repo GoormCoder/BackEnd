413578bbcd8ddb604cad2def5d7bb014b670e3e5
SJ70 <seojae70@gmail.com>
2024-06-17 17:44:57 +0900

    refactor: Comment 관련 상수 분리

---
+++ src/main/java/goormcoder/webide/constants/CommentConstants.java
@@ -0,0 +1,11 @@
+package goormcoder.webide.constants;
+
+public final class CommentConstants {
+
+    public final static int COMMENT_MAX_LENGTH = 4000;
+
+    public final static String COMMENT_OVER_LENGTH = "댓글 내용이 최대 글자 수(" + COMMENT_MAX_LENGTH + ")를 초과했습니다.";
+
+    public final static String COMMENT_IS_BLANK = "댓글 내용은 필수로 입력해야 합니다.";
+
+}
