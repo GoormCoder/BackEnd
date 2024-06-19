00451ec122ba62bef0c033441a4e3f6456a66aad
SJ70 <seojae70@gmail.com>
2024-06-19 17:55:21 +0900

    feat: 문제에 대한 전체 제출 조회 api

--- src/main/java/goormcoder/webide/controller/SolveController.java
+++ src/main/java/goormcoder/webide/controller/SolveController.java
@@ -4,6 +4,7 @@ import goormcoder.webide.domain.Member;
 import goormcoder.webide.domain.Question;
 import goormcoder.webide.domain.Solve;
 import goormcoder.webide.dto.request.SolveCreateDto;
+import goormcoder.webide.dto.response.SolveResponseDto;
 import goormcoder.webide.dto.response.SolveSummaryDto;
 import goormcoder.webide.jwt.PrincipalHandler;
 import goormcoder.webide.service.MemberService;
@@ -11,8 +12,10 @@ import goormcoder.webide.service.QuestionService;
 import goormcoder.webide.service.SolveService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +39,21 @@ public class SolveController {
         Question question = questionService.findById(questionId);
         Solve solve = solveService.create(question, member, createDto);
         return ResponseEntity.ok(SolveSummaryDto.of(solve));
+    }
+
+    @GetMapping("/{solveId}")
+    @Operation(summary = "제출 열람", description = "본인 혹은 맞은 사람만 열람 가능")
+    public ResponseEntity<SolveResponseDto> getSolve(@PathVariable Long solveId) {
+        Member member = memberService.findByLoginId(principalHandler.getMemberLoginId());
+        Solve solve = solveService.findById(solveId);
+        solveService.checkCanAccess(solve, member);
+        return ResponseEntity.ok(SolveResponseDto.of(solve));
+    }
+
+    @GetMapping("/{questionId}")
+    @Operation(summary = "제출 조회", description = "문제에 대한 모든 제출 조회")
+    public ResponseEntity<List<SolveSummaryDto>> getAllSolves(@PathVariable Long questionId) {
+        return ResponseEntity.ok(questionService.findSolvesById(questionId));
     }
 
 }
--- src/main/java/goormcoder/webide/service/SolveService.java
+++ src/main/java/goormcoder/webide/service/SolveService.java
@@ -1,14 +1,18 @@
 package goormcoder.webide.service;
 
+import goormcoder.webide.constants.ErrorMessages;
 import goormcoder.webide.domain.Member;
 import goormcoder.webide.domain.Question;
 import goormcoder.webide.domain.Solve;
+import goormcoder.webide.domain.enumeration.MemberRole;
 import goormcoder.webide.domain.enumeration.SolveResult;
 import goormcoder.webide.dto.TestCaseValueDto;
 import goormcoder.webide.dto.request.SolveCreateDto;
 import goormcoder.webide.repository.SolveRepository;
 import goormcoder.webide.util.javaCompiler.ScoringUtil;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -39,6 +43,20 @@ public class SolveService {
         solveRepository.save(solve);
 
         return solve;
+    }
+
+    public Solve findById(Long id) {
+        return solveRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorMessages.SOLVE_NOT_FOUND.getMessage()));
+    }
+
+    public void checkCanAccess(Solve solve, Member member) {
+        if (member.equals(solve.getMember()) || member.getRole().equals(MemberRole.ROLE_ADMIN)) {
+            return;
+        }
+        if (solveRepository.findAllByIdAndMember(solve.getId(), member).isEmpty()) {
+            throw new AccessDeniedException(ErrorMessages.FORBIDDEN_SOLVE_ACCESS.getMessage());
+        }
     }
 
 }
