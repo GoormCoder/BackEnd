d803c0fcfdc958630aa70df8a992ec95c66c318c
ISMINIMIN <summmm.224@gmail.com>
2024-06-20 22:54:10 +0900

    refactor: JwtTokenDto 필드 추가

--- src/main/java/goormcoder/webide/dto/response/JwtTokenDto.java
+++ src/main/java/goormcoder/webide/dto/response/JwtTokenDto.java
@@ -1,15 +1,23 @@
 package goormcoder.webide.dto.response;
 
+import goormcoder.webide.domain.Member;
+
 public record JwtTokenDto(
 
         String loginId,
+        String nickname,
         String accessToken,
         String refreshToken
 
 ) {
 
-    public static JwtTokenDto of(String loginId, String accessToken, String refreshToken) {
-        return new JwtTokenDto(loginId, accessToken, refreshToken);
+    public static JwtTokenDto of(Member member, String accessToken, String refreshToken) {
+        return new JwtTokenDto(
+                member.getLoginId(),
+                member.getNick(),
+                accessToken,
+                refreshToken
+        );
     }
 
 }
