5168040e28eebbad82b245aec1834fec661df011
SJ70 <seojae70@gmail.com>
2024-06-20 16:11:33 +0900

    fix: 태그명이 고유하도록 수정

--- src/main/java/goormcoder/webide/domain/QuestionTag.java
+++ src/main/java/goormcoder/webide/domain/QuestionTag.java
@@ -23,7 +23,7 @@ public class QuestionTag {
     @Column(name = "tag_id")
     private Long id;
 
-    @Column(name = "tag_name")
+    @Column(name = "tag_name", nullable = false, unique = true)
     private String name;
 
     @ManyToMany(mappedBy = "tags")
