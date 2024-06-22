9f42d973c9c9572da8d4bac0fa549343e5af5a0d
ISMINIMIN <summmm.224@gmail.com>
2024-06-23 06:29:12 +0900

    refactor: 헤더에서 아이디 추출하여 사용

--- src/main/java/goormcoder/webide/dto/request/ChatMessageSendDto.java
+++ src/main/java/goormcoder/webide/dto/request/ChatMessageSendDto.java
@@ -8,9 +8,6 @@ public record ChatMessageSendDto(
         @NotNull(message = ChatConstants.CHATROOM_ID_IS_NULL)
         Long chatRoomId,
 
-        // Web Socket 테스트용 필드 - 추후 삭제 예정
-        String senderLoginId,
-
         @NotNull(message = ChatConstants.MESSAGE_IS_NULL)
         String message
 
